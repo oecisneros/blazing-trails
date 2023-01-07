eb53a40ec4d4ca1e4b1f2abb1c216dabd0c1af20
cisneroso <cisneroso@globalhitss.com>
2023-01-07 14:03:56 -0600

    Initial commit - Chapters 1-4

---
+++ src/BlazingTrails.Client/Extensions/CoreExtensions.cs
@@ -0,0 +1,9 @@
+﻿namespace BlazingTrails.Client;
+
+public static class CoreExtensions
+{
+    public static bool GreaterThanZero(this int value) => value > 0;
+
+    public static bool Contains(this string str, string value, bool ignoreCase) =>
+        str.Contains(value, ignoreCase ? StringComparison.CurrentCultureIgnoreCase : StringComparison.CurrentCulture);
+}
---
+++ src/BlazingTrails.Client/Providers/TrailProvider.cs
@@ -0,0 +1,18 @@
+﻿using System.Net.Http.Json;
+using BlazingTrails.Client.Features.Home;
+
+namespace BlazingTrails.Client.Providers;
+
+public interface ITrailProvider
+{
+    Task<IEnumerable<Trail>?> GetTrailsAsync();
+}
+
+public sealed record TrailProvider(HttpClient Client) :
+    ITrailProvider
+{
+    public async Task<IEnumerable<Trail>?> GetTrailsAsync()
+    {
+        return await Client.GetFromJsonAsync<IEnumerable<Trail>>("trails/trail-data.json").ConfigureAwait(false);
+    }
+}
